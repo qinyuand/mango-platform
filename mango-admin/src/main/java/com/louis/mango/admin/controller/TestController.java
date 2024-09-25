146ba674e531d1859258905299345d8d11c88efb
qinyuand <qinyuand@yeah.net>
2024-09-25 23:49:11 +0800

    branch1

---
+++ mango-admin/src/main/java/com/louis/mango/admin/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.louis.mango.admin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class TestController {
+
+    @RequestMapping("/git")
+    public String git() throws Exception {
+        return "hello l3 -> king -> idea  -> branch";
+    }
+
+
+}
